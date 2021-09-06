39154d410a4ed933052efbe3e3cdfc15ce00fcab
EDnotSheeran <juninho.tab3@gmail.com>
2021-09-06 22:14:13 +0000

    Deploying to gh-pages from @ EDnotSheeran/Tailwind-Blog@ff155256cb7d5a86dfc058b0bbdd9de29a255b13 🚀

--- _next/static/wl-3ynkwnLzk_wrpLP4yY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0cda042c802181d1e278.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
